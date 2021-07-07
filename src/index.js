a66d9cbe96a9b18fd15805404fc16cb1a039148d
SK-Runner <sukun1140@163.com>
2021-07-07 19:54:57 +0800

    1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import toTokens from './templateToTokens.js'
 import toTemplate from './toTemplate.js'
 window.MyMustache = {
   render(templateStr,data){
-    // 模板字符穿转化为tokens
+    // 模板字符串转化为tokens
     var tokens = toTokens(templateStr)
     console.log(tokens);
     // 数据注入
